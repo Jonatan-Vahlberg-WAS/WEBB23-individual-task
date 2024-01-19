708076fee226bd5c27c16f600b85732ee46ec97c
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2024-01-19 09:56:32 +0100

    basic setup for backend

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+if (process.env.NODE_ENV === 'development') {
+    app.use(cors({
+        origin: "*"
+    }));
+}
+else {
+    app.use(cors({
+        origin: ['http://localhost:5500', 'http://127.0.0.1:5500']
+    }));
+}
